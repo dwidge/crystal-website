19d846f6c842b3c1a40c51560f1e2b2821f5a381
dwidge <dwidge+gh@gmail.com>
2023-09-21 13:44:58 +0200

    Add Carousel

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,6 +5,8 @@ import { BasketList } from "../types/BasketList.js";
 import { EveryOccasionSection } from "./EveryOccasionSection.js";
 import { CaptureMemoriesSection } from "./CaptureMemoriesSection.js";
 import { Product } from "../types/Product.js";
+import { Carousel } from "./Carousel.js";
+import { app } from "../config.js";
 
 export const Home: React.FC<{
   basketState: State<BasketList>;
@@ -13,6 +15,12 @@ export const Home: React.FC<{
   return (
     <>
       {CaptureMemoriesSection()}
+      {Carousel({
+        imgs: items.map((item) => ({
+          src: app.root + "/data/" + item.imageId,
+          alt: item.name,
+        })),
+      })}
       {ProductList({ basketState: [basketList], items })}
       {EveryOccasionSection()}
     </>
